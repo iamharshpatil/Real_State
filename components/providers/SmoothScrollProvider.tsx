259b040635e60b8e50b27f437ccaf2098a826ed0
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2026-01-19 23:56:38 +0530

    fix: update Lenis import path in SmoothScrollProvider and package.json

--- components/providers/SmoothScrollProvider.tsx
+++ components/providers/SmoothScrollProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function SmoothScrollProvider({
   children,
